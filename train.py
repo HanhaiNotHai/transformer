02509274606481d5d12984cfea66ca4aa1951433
Hanhai <1932232849@qq.com>
2024-09-09 21:18:41 +0800

    add mem ussage at tqdm pbar

--- train.py
+++ train.py
@@ -131,40 +131,46 @@ def main() -> None:
     for epoch in trange(config.epochs, desc='epoch'):
         wandb.log({'epoch': epoch}) if config.WANDB else None
 
-        for batch in tqdm(train_dataloader, 'train', leave=False):
-            step += 1
-            x, x_pad_mask, y, y_pad_mask = to(batch, transformer.device)
-            # [b, l - 1]
-            target = y[:, 1:]
-            y = y[:, :-1]
-            y_pad_mask = y_pad_mask[:, :-1]
-
-            optimizer.zero_grad()
-            logits = transformer(x, y, x_pad_mask, y_pad_mask)
-            # [b, l - 1, vocab_size] -> [b, vocab_size, l - 1]
-            logits.transpose_(1, 2)
-            loss: Tensor = cross_entropy_loss(logits, target)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-
-            if config.WANDB:
-                wandb.log(
-                    {'train_loss': loss, 'learning_rate': optimizer.param_groups[0]['lr']}, step
+        with tqdm(train_dataloader, desc='train', leave=False) as pbar:
+            for batch in pbar:
+                pbar.set_postfix_str(
+                    f'{torch.mps.current_allocated_memory() / 2**30:.1f} / '
+                    f'{torch.mps.driver_allocated_memory() / 2**30:.1f} GB'
                 )
+                step += 1
+                x, x_pad_mask, y, y_pad_mask = to(batch, transformer.device)
+                # [b, l - 1]
+                target = y[:, 1:]
+                y = y[:, :-1]
+                y_pad_mask = y_pad_mask[:, :-1]
 
-            if step % config.eval_save_per_steps == 0:
-                transformer.eval()
-                for x, _, tgt in tqdm(test_dataloader, 'test', leave=False):
-                    x: Tensor = x.to(transformer.device)
-                    y_hat = transformer.inference(x)
-                    pred = tokenizer.decode(y_hat)
-                    bleu.add_batch(predictions=[pred], references=[tgt])
-                transformer.train()
+                optimizer.zero_grad()
+                logits = transformer(x, y, x_pad_mask, y_pad_mask)
+                # [b, l - 1, vocab_size] -> [b, vocab_size, l - 1]
+                logits.transpose_(1, 2)
+                loss: Tensor = cross_entropy_loss(logits, target)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
 
-                score = bleu.compute()['score']
-                wandb.log({'bleu': score}, step) if config.WANDB else None
-                saver.save(score, epoch, step)
+                if config.WANDB:
+                    wandb.log(
+                        {'train_loss': loss, 'learning_rate': optimizer.param_groups[0]['lr']},
+                        step,
+                    )
+
+                if step % config.eval_save_per_steps == 0:
+                    transformer.eval()
+                    for x, _, tgt in tqdm(test_dataloader, 'test', leave=False):
+                        x: Tensor = x.to(transformer.device)
+                        y_hat = transformer.inference(x)
+                        pred = tokenizer.decode(y_hat)
+                        bleu.add_batch(predictions=[pred], references=[tgt])
+                    transformer.train()
+
+                    score = bleu.compute()['score']
+                    wandb.log({'bleu': score}, step) if config.WANDB else None
+                    saver.save(score, epoch, step)
 
     wandb.finish() if config.WANDB else None
 
